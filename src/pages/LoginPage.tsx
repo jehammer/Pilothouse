4197f1d8be723f3fa490d12c9194a55000be4a83
Joshua Hammer <jebelfor@gmail.com>
2023-06-03 18:59:31 -0500

    Quick Login

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -9,21 +9,21 @@ function LoginPage() {
     console.log('Data saved:', data);
   };
 
-  const loadData = (): void => {
+  // SAVING THIS FOR QUICK REUSE LATER
+  /*const loadData = (): void => {
     const savedData = localStorage.getItem('loginData');
     if (savedData) {
       setData(savedData);
       console.log('Data loaded:', savedData);
     }
-  };
+  };*/
 
   return (
     <div className='loginPage'>
       <h3>Greetings Commander</h3>
       <h4>Enter your ID to access the system</h4>
       <input type="text" value={data} onChange={(e) => setData(e.target.value)} />
-      <button onClick={saveData}>Save Data</button>
-      <button onClick={loadData}>Load Data</button>
+      <button onClick={saveData}>Launch</button>
     </div>
   );
 }
