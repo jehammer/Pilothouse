3293cb7e4ce22a6c0960121c1656e79f062537cc
Joshua Hammer <jebelfor@gmail.com>
2023-06-04 22:27:56 -0500

    Implement basic Registration layout

--- src/pages/RegisterPage.tsx
+++ src/pages/RegisterPage.tsx
@@ -1,19 +1,23 @@
 import React, { useState } from 'react';
 import stservice from '../components/stservice';
+import { useNavigate } from 'react-router-dom';
+import '../css/registration.css';
 
 function RegisterPage() {
   const [data, callsign] = useState('');
+  const navigate = useNavigate();
 
   const service = new stservice();
 
   const register = (): void => {
     //console.log('Registered Callsign:', data);
     service.test(data);
+    navigate('/BridgePage');
   };
 
   return (
-    <div>
-      <p>Welcome to Registration!</p>
+    <div className='registration'>
+      <p>Enter your callsign, Commander</p>
       <input type="text" value={data} onChange={(e) => callsign(e.target.value)} />
       <button onClick={register}>Register</button>
     </div>
